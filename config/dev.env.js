d21f4949fa64cd4160ad38a55baa0afcbb876ea9
toast38coza <info@38.co.za>
2017-03-14 10:45:18 +0200

    update base_url for production

--- config/dev.env.js
+++ config/dev.env.js
@@ -3,5 +3,5 @@ var prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  API_ROOT: '"http://localhost:8000"' // nb: no trailing slash
+  API_ROOT: '"http://api.appointmentguru.co"' // nb: no trailing slash
 })
